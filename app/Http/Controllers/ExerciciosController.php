fea5c5a0ae778c2c6e4574df82bed68cc5f52196
ramosjean <ramosjean233@gmail.com>
2026-08-25 15:22:51 -0300

    exercicios 3 e 4

--- app/Http/Controllers/ExerciciosController.php
+++ app/Http/Controllers/ExerciciosController.php
@@ -27,4 +27,31 @@ class ExerciciosController extends Controller
         $sub = $valor1 - $valor2;
         return view('exer2', ['sub' => $sub]);
     }
+
+    public function abrirFormExer3(){
+        return view('exer3');
+    }
+
+    public function respostaExer3(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $mult = $valor1 * $valor2;
+        return view('exer3', ['mult' => $mult]);
+    }
+
+    public function abrirFormExer4(){
+        return view('exer4');
+    }
+
+    public function respostaExer4(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+            if ($valor2 == 0){
+                return view('exer4', ['erro' => 'Não é possivel dividir por zero.']);
+            }
+        $div = $valor1  / $valor2;
+        return view('exer4', ['div' => $div]);
+    }
+
+
 }
---
+++ resources/views/exer4.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+@section('titulo', 'Exercicio 4')
+@section('conteudo')
+        <h1>Exercício 4</h1>
+        <form method="post" action="/exer4resp">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Informe o primeiro valor</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor2" class="form-label">Informe o segundo valor</label>
+                <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        @isset($div)
+            <p>A Divisão é: {{ $div }}</p>
+        @endisset
+@endsection   
